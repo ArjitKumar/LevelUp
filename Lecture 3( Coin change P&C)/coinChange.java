d6c14131f5223fc868f9fb4218385c97e4a30d84
ArjitKumar <ajju9990.ak@gmail.com>
2022-04-02 13:43:03 +0530

    Added coin change prbolems

---
+++ Lecture 3( Coin change P&C)/coinChange.java
@@ -0,0 +1,212 @@
+
+    //   ======================== 8 variations of tree and coin change permutation ==================================
+
+import java.util.*;
+
+public class coinChange {
+
+    public static int coinChangePermutaion_Inf(int[] coin ,  int tar , String psf) {
+         
+        if(tar == 0) {
+            System.out.println(psf);
+            return 1;
+        }
+
+        int count = 0;
+
+            for( int i = 0 ; i < coin.length ; i++) {
+                if( tar - coin[i] >= 0) {
+                    count += coinChangePermutaion_Inf(coin, tar - coin[i], psf + coin[i] + " ");
+                }
+            }
+        
+        return count;
+    }
+
+
+    // idx playes a important role here
+
+    public static int coinChangeCombination_Inf(int[] coin ,  int tar ,int idx ,String psf) {
+         
+        if(tar == 0) {
+            System.out.println(psf);
+            return 1;
+        }
+
+        int count = 0;
+
+            for( int i = idx ; i < coin.length ; i++) {
+                if( tar - coin[i] >= 0) {
+                    count += coinChangeCombination_Inf(coin, tar - coin[i],i, psf + coin[i] + " ");
+                }
+            }
+        
+        return count;
+    }
+
+
+    public static int coinChangeCombination_Single(int[] coin ,  int tar ,int idx ,String psf) {
+         
+        if(tar == 0) {
+            System.out.println(psf);
+            return 1;
+        }
+
+        int count = 0;
+
+            for( int i = idx ; i < coin.length ; i++) {
+                if( tar - coin[i] >= 0) {
+                    count += coinChangeCombination_Single(coin, tar - coin[i],i+1, psf + coin[i] + " ");
+                }
+            }
+        
+        return count;
+    }
+
+       // use visited array
+    public static int coinChangePermutaion_Single(int[] coin , int tar ,String psf) {
+         
+        if(tar == 0) {
+            System.out.println(psf);
+            return 1;
+        }
+
+        int count = 0;
+
+            for( int i = 0 ; i < coin.length ; i++) {
+                if( coin[i] > 0 && tar - coin[i] >= 0) {
+                    int val = coin[i];
+                    coin[i] = -coin[i];  // making negative
+
+                    count += coinChangePermutaion_Single(coin, tar - val,psf + val + " ");
+
+                    coin[i] = -coin[i]; // making positive
+                }
+            }
+        
+        return count;
+    }
+
+
+    // Coin change combination single coin using binary subsequence ===============================================
+
+    public static int coinChangecombination_Single_Using_Binary_Subsequence(int[] coin , int tar ,int idx, String psf) {
+         
+        if(tar == 0 || idx == coin.length) { // 2 basecases
+            if( tar == 0) {
+                System.out.println(psf);       // we get our answer when target is 0
+
+                return 1;
+            }  
+            return 0;
+           
+        }
+
+        int count = 0;
+        if( tar - coin[idx] >= 0) {
+            count += coinChangecombination_Single_Using_Binary_Subsequence(coin, tar - coin[idx], idx + 1, psf + coin[idx] + " ");
+           
+        }
+        count += coinChangecombination_Single_Using_Binary_Subsequence(coin, tar, idx + 1, psf);
+
+        return count;
+    }
+
+
+    public static int coinChangecombination_Infinite_Using_Binary_Subsequence(int[] coin , int tar ,int idx, String psf) {
+         
+        if(tar == 0 || idx == coin.length) { // 2 basecases
+            if( tar == 0) {
+                System.out.println(psf);       // we get our answer when target is 0
+
+                return 1;
+            }  
+            return 0;
+           
+        }
+
+        int count = 0;
+        if( tar - coin[idx] >= 0) {
+            count += coinChangecombination_Infinite_Using_Binary_Subsequence(coin, tar - coin[idx], idx , psf + coin[idx] + " ");
+           
+        }
+        count += coinChangecombination_Infinite_Using_Binary_Subsequence(coin, tar, idx + 1, psf);
+
+        return count;
+    }
+
+    public static int coinChangePermutaion_Infinte_Using_Binary_Subsequence(int[] coin , int tar ,int idx, String psf) {
+         
+        if(tar == 0 || idx == coin.length) { // 2 basecases
+            if( tar == 0) {
+                System.out.println(psf);       // we get our answer when target is 0
+
+                return 1;
+            }  
+            return 0;
+           
+        }
+
+        int count = 0;
+        if( tar - coin[idx] >= 0) {
+            // passing 0 kyinki sb coins aa skte hain
+
+            count += coinChangePermutaion_Infinte_Using_Binary_Subsequence(coin, tar - coin[idx], 0, psf + coin[idx] + " ");
+           
+        }
+        count += coinChangePermutaion_Infinte_Using_Binary_Subsequence(coin, tar, idx + 1, psf);
+
+        return count;
+    }
+    
+    
+    
+    
+    public static int coinChangePermutaion_Single_Using_Binary_Subsequence(int[] coin , int tar ,int idx, String psf) {
+         
+        if(tar == 0 || idx == coin.length) { // 2 basecases
+            if( tar == 0) {
+                System.out.println(psf);       // we get our answer when target is 0
+
+                return 1;
+            }  
+            return 0;
+           
+        }
+
+        int count = 0;
+        if( coin[idx] > 0 && tar - coin[idx] >= 0) {
+            // passing 0 kyinki sb coins aa skte hain
+            int val = coin[idx];
+            coin[idx] = -coin[idx];  // making negative
+
+            count += coinChangePermutaion_Single_Using_Binary_Subsequence(coin, tar - val, 0, psf + val + " ");
+
+            coin[idx] = -coin[idx]; // making positive
+
+           
+        }
+        count += coinChangePermutaion_Single_Using_Binary_Subsequence(coin, tar, idx + 1, psf);
+
+        return count;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+        int[] coin = {2,3,5,7};
+        // System.out.println(coinChangePermutaion_Inf(coin,10,""));
+        // System.out.println(coinChangeCombination_Inf(coin,10,0,""));
+        // System.out.println(coinChangeCombination_Single(coin,10,0,"")); 
+        // System.out.println(coinChangePermutaion_Single(coin,10,""));
+        // System.out.println(coinChangecombination_Single_Using_Binary_Subsequence(coin,10,0,""));
+        // System.out.println(coinChangePermutaion_Infinte_Using_Binary_Subsequence(coin,10,0,""));
+        // System.out.println(coinChangePermutaion_Infinte_Using_Binary_Subsequence(coin,10,0,""));
+        System.out.println(coinChangePermutaion_Single_Using_Binary_Subsequence(coin,10,0,""));
+
+    }
+}
--- mazePath.java
+++ mazePath.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class mazePath{
 
-    public static int mazePath( int sr, int sc , int er, int ec , int[][] dir, String[] dis, String psf){
+    public static int mazePath( int sr, int sc , int er, int ec , int[][] dir, String[] dis, String psf,int[][] matrix){
 
-        if( sr == er &&  sc == ec){
+    if( sr == er &&  sc == ec){
       System.out.println( psf);
       return 1;
         }
-
+        
+        matrix[sr][sc] -= 1;
         int count = 0;
 
         for( int d = 0; d < dir.length ; d++) {
@@ -16,7 +17,8 @@ public class mazePath{
             int c = sc + dir[d][1]; // for column
 
             if( r >= 0 && c >= 0 && r<= er && c <= ec) {
-                count += mazePath(r,c,er,ec,dir,dis,psf + dis[d]);
+                matrix[r][c] = matrix[r][c] - 1;
+                count += mazePath(r,c,er,ec,dir,dis,psf + dis[d], matrix);
             }
         }
 
@@ -25,11 +27,28 @@ public class mazePath{
 
     public static void main( String[] args){
       
-          int[][] d = {{0,1},{1,0},{1,1}};
-          String[] dis = {"H","V","D"};
+          int[][] d = {{0,1},{1,0}};
+          String[] dis = {"H","V"};
+          int[][] matrix = {{3,2},{1,3}};
 
-          System.out.println(mazePath(0,0,2,2,d,dis,""));
-
+          int count = (mazePath(0,0,matrix.length - 1,matrix[0].length - 1,d,dis,"", matrix));
+        //   System.out.println(count);
+        //   int ans = 0;
+          for( int i = 0 ; i < matrix.length ; i++) {
+              for( int j = 0 ; j < matrix[0].length; j++) {
+                        //    if(matrix[i][j] != 0) {
+                        //             ans++;
+                        //             break;
+                        //    }
+                        System.out.print(matrix[i][j] + " ");
+              }
+              System.out.println();
+          }
+        // if( ans != 0) {
+        //     System.out.println("No");
+        // }else{
+        //     System.out.println("Yes");
+        // }
 
     }
 
